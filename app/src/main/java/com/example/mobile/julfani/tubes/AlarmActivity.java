ba23c9984c64b1afca8fdf1cc265e1bee958c6dc
Julfani <zulfani.akbar123@gmail.com>
2022-06-24 20:40:09 +0800

    homepage to second page

---
+++ app/src/main/java/com/example/mobile/julfani/tubes/AlarmActivity.java
@@ -0,0 +1,13 @@
+package com.example.mobile.julfani.tubes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
